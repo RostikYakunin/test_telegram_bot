4d580eda1d8bcde9d848301dcc6f7ad99b9831ba
Rostyslav Yakunin <yakunin.dev.official@gmail.com>
2023-09-22 14:16:05 +0300

    initial commit

---
+++ src/main/java/com/example/test_telegram_bot/TestTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTelegramBotApplication.class, args);
+    }
+
+}
